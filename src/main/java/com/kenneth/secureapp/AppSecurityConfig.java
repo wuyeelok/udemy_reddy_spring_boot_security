6617cda49af824d64546459b30add9943bd958f5
wuyeelok <wuyeelok@gmail.com>
2019-11-16 09:50:52 +0800

    Comment out no use code

--- src/main/java/com/kenneth/secureapp/AppSecurityConfig.java
+++ src/main/java/com/kenneth/secureapp/AppSecurityConfig.java
@@ -1,30 +1,23 @@
 package com.kenneth.secureapp;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		
-		List<UserDetails> users = new ArrayList<>();
-		users.add(User.withDefaultPasswordEncoder().username("kenneth").password("1234").roles("USER").build());
-		
-		return new InMemoryUserDetailsManager(users);
-	}
-	
-	
+	/*
+	 * @Bean
+	 * 
+	 * @Override protected UserDetailsService userDetailsService() {
+	 * 
+	 * List<UserDetails> users = new ArrayList<>();
+	 * users.add(User.withDefaultPasswordEncoder().username("kenneth").password(
+	 * "1234").roles("USER").build());
+	 * 
+	 * return new InMemoryUserDetailsManager(users); }
+	 */
+
 }
